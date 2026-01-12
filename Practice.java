f459dc3431a0727a76a5430d3c9fadea4372fdb9
Shreya Shristi <shristishreyac@gmail.com>
2026-01-12 18:46:23 +0530

    Done with basic recursion uestions

--- Practice.java
+++ Practice.java
@@ -17,7 +17,13 @@ class Practice {
         System.out.println("Reversed of 7842 is "+reverseD(7842,0));
         System.out.println(checkPalin(11131));
         int[] a={2,32,3,10,11,-232};
+        int[] values={1,2,3,4,5,5,6,7,8,8,5};
         System.out.println("Sum of The given elements of array is: "+arraySum(a,0,0));
+        System.out.println("Elements of the array are:");
+        traverse(values,0);
+        System.out.println("\nMax of all the elements in the array: "+max(values,0,1));
+        System.out.println("Is the array sorted?"+sortCheck(a,0,false));
+        System.out.println("Total occurences of the given element in the array is "+occur(values,5,0,0));
     }
     public static int Sum(int n){
         if(n==0) return 0;
@@ -74,6 +80,34 @@ class Practice {
     }
     public static int arraySum(int[] a,int n,int s){
         if(n==a.length)return s;
-        return s+a[n]+arraySum(a,++n,s); 
+        return a[n]+arraySum(a,++n,s); 
+    }
+       static void traverse(int[] a,int i){
+        int n=a.length;
+       // int i=0;
+       if(i==n)return;
+       System.out.print(a[i]+" ");
+       traverse(a,i+1);
+    }
+    static int max(int[] a,int max,int i){
+        int n=a.length;
+        if(i==n)return max;
+        if(a[i]>max){
+            max=a[i];
+        }
+        return max(a,max,i+1);
+        //return max;
+
+    }
+    static boolean sortCheck(int[] a,int c,boolean f){
+        if(c==a.length-1) return true;
+        if((a[c]>a[c+1]))return false;
+        return sortCheck(a,c+1,f);
+    }
+    static int occur(int[] a,int target,int i,int c){
+        if(i==a.length)return c;
+        if(a[i]==target)c++;
+        return occur(a,target,i+1,c);
+
     }
 }
